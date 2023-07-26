737beb2cb9056fd3693ac88c26e2874c380d038c
KarinaSofia <104159293+KarinaSofia@users.noreply.github.com>
2023-07-26 15:23:15 -0400

    Login Token Started

--- customers-service/src/main/java/com/mc_website/customersservice/datalayer/CustomerRepository.java
+++ customers-service/src/main/java/com/mc_website/customersservice/datalayer/CustomerRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     Customer findByCustomerIdentifier_CustomerId(String customerId);
+
+    Customer findByEmail(String email);
     boolean existsByCustomerIdentifier_CustomerId(String customerId);
 
 }
