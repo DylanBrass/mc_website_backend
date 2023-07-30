10dd1bd84c81d9c1abb97db60ef72bfbcc640450
Mila5847 <mila.kehayova@abv.bg>
2023-07-30 19:35:45 -0400

    allowing CORS

--- api-gateway/src/main/java/com/mc_website/apigateway/presentation/Customer/CustomerController.java
+++ api-gateway/src/main/java/com/mc_website/apigateway/presentation/Customer/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/customers")
 @CrossOrigin(origins = "http://localhost:3000")
+
 public class CustomerController {
     CustomersService customerService;
     public CustomerController(CustomersService customerService) {
