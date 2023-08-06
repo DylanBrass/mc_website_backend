56d62accaa8282a6eb275f59beccfd6e6fb50054
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2023-08-06 15:10:49 -0400

    Made Eureka server

--- api-gateway/src/main/java/com/mc_website/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mc_website/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,10 @@ package com.mc_website.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class ApiGatewayApplication {
 	@Bean
 	RestTemplate restTemplate()
